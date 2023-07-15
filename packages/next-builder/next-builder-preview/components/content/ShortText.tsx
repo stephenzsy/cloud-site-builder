dd3868946ffedbbfdd444bd48f611ca49060be4c
Stephen Zhang <stzh@microsoft.com>
2023-07-14 23:26:52 -0500

    update

--- packages/next-builder/next-builder-preview/components/content/ShortText.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentContentShortText } from "@/lib/generated/graphql";
-
-export default function ShortText({
-  content,
-}: {
-  content: ComponentContentShortText;
-}) {
-  return content.value;
-}
--- packages/next-builder/next-builder-preview/components/types.ts
+++
@@ -1,5 +0,0 @@
-import { ComponentContentShortText, ComponentContentSvgIcon, Error } from "@/lib/generated/graphql";
-
-export type ComponentContentType = Pick<ComponentContentShortText | ComponentContentSvgIcon, "__typename" | "targets">
-
-export type ComponentContentTypeName = string & ComponentContentType["__typename"]
