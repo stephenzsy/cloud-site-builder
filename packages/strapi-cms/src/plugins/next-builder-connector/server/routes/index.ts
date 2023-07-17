1c2fc1bcadb84e39b30b1bd07def83132b7b6bf1
Stephen Zhang <stephen.zsy@gmail.com>
2023-07-17 14:20:39 -0500

    fix routes

--- packages/strapi-cms/src/plugins/next-builder-connector/server/routes/index.ts
+++ packages/strapi-cms/src/plugins/next-builder-connector/server/routes/index.ts
@@ -1,17 +1,1 @@
-export default [
-  {
-    method: "GET",
-    path: "/token",
-    handler: "token.getToken",
-    config: {
-      policies: [
-        {
-          name: "admin::hasPermissions",
-          config: {
-            actions: ["plugin::next-builder-connector.preview"],
-          },
-        },
-      ],
-    },
-  },
-];
+export default [];
