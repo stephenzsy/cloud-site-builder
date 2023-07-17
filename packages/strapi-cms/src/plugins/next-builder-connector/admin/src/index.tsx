d86a9648a1937defdddf8757aa9e4fcb0313a6c7
Stephen Zhang <stephen.zsy@gmail.com>
2023-07-17 16:09:11 -0500

    things

--- packages/strapi-cms/src/plugins/next-builder-connector/admin/src/index.tsx
+++ packages/strapi-cms/src/plugins/next-builder-connector/admin/src/index.tsx
@@ -1,13 +1,22 @@
 import { prefixPluginTranslations } from "@strapi/helper-plugin";
 
 import pluginPkg from "../../package.json";
-import pluginId from "./pluginId";
 import Initializer from "./components/Initializer";
-import PluginIcon from "./components/PluginIcon";
+import pluginId from "./pluginId";
 
 const name = pluginPkg.strapi.name;
 
 export default {
+  register(app: any) {
+    const plugin = {
+      id: pluginId,
+      initializer: Initializer,
+      isReady: false,
+      name,
+    };
+
+    app.registerPlugin(plugin);
+  },
   bootstrap(app: any) {},
 
   async registerTrads(app: any) {
